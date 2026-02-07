1f2c8c46e2c76cf238d42c4265b5984c1593429b
Ignacio <ignacio@Ignacios-MacBook-Pro.local>
2026-02-07 10:16:30 +0100

    ex2: extract to file

---
+++ 02-performance-allocation/02-concurrent-map-with-sharded-locks/sharded_map.go
@@ -0,0 +1,91 @@
+package sharded_map
+
+import (
+	"errors"
+	"fmt"
+	"hash/fnv"
+	"sync"
+)
+
+const shardNumber = 32
+
+type ShardedMap[K comparable, V any] struct {
+	shards  []map[K]V
+	mutexes []sync.RWMutex
+}
+
+func NewShardedMap[K comparable, V any](options ...ShardedMapOption[K, V]) (*ShardedMap[K, V], error) {
+	sm := &ShardedMap[K, V]{
+		shards:  make([]map[K]V, shardNumber),
+		mutexes: make([]sync.RWMutex, shardNumber),
+	}
+	for i := range sm.shards {
+		sm.shards[i] = make(map[K]V)
+	}
+
+	for _, opt := range options {
+		if err := opt(sm); err != nil {
+			return nil, fmt.Errorf("can't create ShardedMap: %v", err)
+		}
+	}
+	return sm, nil
+}
+
+func (s *ShardedMap[K, V]) Set(k K, v V) {
+	var hash uint64
+	switch k := any(k).(type) {
+	case string:
+		fnv := fnv.New64a()
+		fnv.Write([]byte(k))
+		hash = fnv.Sum64()
+
+	case int:
+		hash = uint64(k)
+
+	default:
+		panic("unreachable")
+	}
+
+	i := int(hash % uint64(len(s.shards)))
+
+	mu := &s.mutexes[i]
+	mu.Lock()
+	defer mu.Unlock()
+
+	m := s.shards[i]
+	m[k] = v
+}
+
+func (s *ShardedMap[K, V]) Keys() []K {
+	var keys []K
+	for i := range s.mutexes {
+		s.mutexes[i].RLock()
+		m := s.shards[i]
+		for k := range m {
+			keys = append(keys, k)
+		}
+		s.mutexes[i].RUnlock()
+	}
+	return keys
+}
+
+type ShardedMapOption[K comparable, V any] func(sm *ShardedMap[K, V]) error
+
+func WithNumberOfShards[K comparable, V any](n int8) ShardedMapOption[K, V] {
+	return func(sm *ShardedMap[K, V]) error {
+		if n <= 0 {
+			return errors.New("number of shards must be greater than zero")
+		}
+		if n == int8(len(sm.shards)) {
+			return nil
+		}
+
+		sm.shards = make([]map[K]V, n)
+		for i := range sm.shards {
+			sm.shards[i] = make(map[K]V)
+		}
+
+		sm.mutexes = make([]sync.RWMutex, n)
+		return nil
+	}
+}
--- 02-performance-allocation/02-concurrent-map-with-sharded-locks/sharded_map_test.go
+++ 02-performance-allocation/02-concurrent-map-with-sharded-locks/sharded_map_test.go
@@ -1,10 +1,6 @@
-package main
+package sharded_map
 
 import (
-	"errors"
-	"fmt"
-	"hash/fnv"
-	"sync"
 	"testing"
 )
 
@@ -18,92 +14,10 @@ import (
 // * [ ] `Delete(key K)` - removes key
 // * [ ] `Keys() []K` - returns all keys (order doesn't matter)
 // * [x] Configurable number of shards at construction
-const SHARD_NUMBER = 32
 
 type userId = int
 
 type user = string
-
-type ShardedMap[K comparable, V any] struct {
-	shards  []map[K]V
-	mutexes []sync.RWMutex
-}
-
-func (s *ShardedMap[K, V]) Set(k K, v V) {
-	var hash uint64
-	switch k := any(k).(type) {
-	case string:
-		fnv := fnv.New64a()
-		fnv.Write([]byte(k))
-		hash = fnv.Sum64()
-
-	case int:
-		hash = uint64(k)
-
-	default:
-		panic("unreachable")
-	}
-
-	i := int(hash % uint64(len(s.shards)))
-
-	mu := &s.mutexes[i]
-	mu.Lock()
-	defer mu.Unlock()
-
-	m := s.shards[i]
-	m[k] = v
-}
-
-func (s *ShardedMap[K, V]) Keys() []K {
-	var keys []K
-	for i := range s.mutexes {
-		s.mutexes[i].RLock()
-		m := s.shards[i]
-		for k := range m {
-			keys = append(keys, k)
-		}
-		s.mutexes[i].RUnlock()
-	}
-	return keys
-}
-
-func NewShardedMap[K comparable, V any](options ...ShardedMapOption[K, V]) (*ShardedMap[K, V], error) {
-	sm := &ShardedMap[K, V]{
-		shards:  make([]map[K]V, SHARD_NUMBER),
-		mutexes: make([]sync.RWMutex, SHARD_NUMBER),
-	}
-	for i := range sm.shards {
-		sm.shards[i] = make(map[K]V)
-	}
-
-	for _, opt := range options {
-		if err := opt(sm); err != nil {
-			return nil, fmt.Errorf("can't create ShardedMap: %v", err)
-		}
-	}
-	return sm, nil
-}
-
-type ShardedMapOption[K comparable, V any] func(sm *ShardedMap[K, V]) error
-
-func WithNumberOfShards[K comparable, V any](n int8) ShardedMapOption[K, V] {
-	return func(sm *ShardedMap[K, V]) error {
-		if n <= 0 {
-			return errors.New("number of shards must be greater than zero")
-		}
-		if n == int8(len(sm.shards)) {
-			return nil
-		}
-
-		sm.shards = make([]map[K]V, n)
-		for i := range sm.shards {
-			sm.shards[i] = make(map[K]V)
-		}
-
-		sm.mutexes = make([]sync.RWMutex, n)
-		return nil
-	}
-}
 
 func TestShardedMapCreation(t *testing.T) {
 	if _, err := NewShardedMap[userId, user](); err != nil {
